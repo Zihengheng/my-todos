f9c30418c50cad0bf3bd6f81f2fb94959c939881
wxh <zwang252325@gmail.com>
2020-12-14 00:37:29 +0800

    add files

--- src/components/layouts.vue
+++ src/components/layouts.vue
@@ -1,34 +1,38 @@
 <template>
   <section class="container" > <!--最外层容器-->
     <section class="menu"> <!--左边的容器-->
-    <h2>Hello menu</h2>
     </section>
     <section class="content-container"><!--右边的容器-->
     </section>
   </section>
 </template>
 <script>
-  export  default {};
+    import menus from './menus';
+    export  default {
+        components:{
+            menus
+        }
+    };
 </script>
 <style>
 .body{
     position: absolute;
-    margin-top:0;
-    padding:0;
-    margin-bottom:0;
+    margin:0 0 0 0;
 }
 .container {
     display: flex;
-    margin-top: 0px 0px 0px 0px;
+    margin-top: 0;
+    margin-bottom:0;
     background-color:#315481;
+    height:800px;
+
 }
 .menu {
     width:30%;
-    margin-top:0px;
-    margin-bottom:0;
-    margin-left:0;
     background-color:#99CCCC;
     opacity:0.5;
+    margin-bottom:0;
+    height:100%;
 }
 .content-container{
     flex:1;

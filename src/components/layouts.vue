3a3fd4a062270887238383e0fef1825993ed5d19
wxh <zwang252325@gmail.com>
2020-12-14 12:18:13 +0800

    fix

--- src/components/layouts.vue
+++ src/components/layouts.vue
@@ -1,19 +1,13 @@
 <template>
   <section class="container" > <!--最外层容器-->
     <section class="menu"> <!--左边的容器-->
-     <menus></menus> <!-- 引用menu组件-->
     </section>
     <section class="content-container"><!--右边的容器-->
     </section>
   </section>
 </template>
 <script>
-    import menus from './menus';
-    export  default {
-        components:{
-            menus
-        }
-    };
+  export  default {};
 </script>
 <style lang="less">
 @import '../common/style/layouts.less';
